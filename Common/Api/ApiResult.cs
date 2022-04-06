9228d559fdb571f3ab1a238d98cf95436f804ff2
M-Ghanizadeh <a.ghanizadeh73@gmail.com>
2022-04-06 07:11:59 +0430

    Add ApiResult To Project For Standard Result

---
+++ Common/Api/ApiResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Api
+{
+    public class ApiResult
+    {
+        public bool IsSuccess { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ApiResult<TData> : ApiResult
+    {
+        public TData Data { get; set; }
+    }
+
+    public enum StatusCode
+    {
+        Success = 1,
+        BadRequest = 2,
+        ServerError = 3,
+        NotFound = 4,
+        NotAuothorized = 5,
+        ListEmpty = 6,
+    }
+}
